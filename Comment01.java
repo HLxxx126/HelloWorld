d34e3cce9dfcbffca91414ab8d0724d47278ebd6
LAPTOP-QN818Q9K\87696 <karroy@naver.com>
2023-02-08 22:53:27 +0800

    no message

---
+++ Comment01.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author 87696
+ * @version 1.0
+ */
+public class Comment01 {
+	public static void main(String[] args) {
+		//单行注释
+		/* 多行注释
+		 * 注释
+		 * 注释
+		 */
+		
+	}
+}
---
+++ Var.java
@@ -0,0 +1,14 @@
+public class Var {
+	public static void main(String[] args) {
+		int age = 20;
+		double score = 88.6;
+		char gender = '男';
+		String name = "jack";
+		System.out.println("人的信息如下");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(score);
+		System.out.println(gender);
+		
+	}
+}
