f780751d41c166c75a16d0b45cf4fb5b1199a8d9
HLxxx126 <karroy@naver.com>
2023-06-30 21:28:45 +0900

    零钱通

---
+++ smallchange/oop/SmallChangeSysApp.java
@@ -0,0 +1,9 @@
+package com.hspedu.smallchange.oop;
+
+import com.hspedu.smallchange.SmallChangeSys;
+
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
